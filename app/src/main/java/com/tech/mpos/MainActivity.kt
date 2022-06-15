ac6b10c7aeba8d501f3e880aa0e3c23ff59f9713
abhijeet <abhijeet.nag@credextechnology.com>
2022-06-15 17:49:46 +0530

    15-06-22 commit

--- app/src/main/java/com/tech/mpos/MainActivity.kt
+++ app/src/main/java/com/tech/mpos/MainActivity.kt
@@ -2,10 +2,27 @@ package com.tech.mpos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar?.setTitle("")
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.action_bar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.add_team_icon -> Toast.makeText(this, "Search Clicked", Toast.LENGTH_SHORT).show()
+            R.id.bell_notification_icon -> Toast.makeText(this, "Refresh Clicked", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
